d0afdf9441459ad887a3e9dc4bbf6b619e57e733
ortizbran001 <ortizbran001@hotmail.com>
2024-02-09 23:26:33 -0600

    CardDetails Done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { faker } from '@faker-js/faker'
-const fullname = faker.person.fullName()
+
 const firstname = faker.person.firstName()
 const lastname = faker.person.lastName()
+const fullname = faker.person.fullName({ firstName: firstname, lastName: lastname })
 const job_title = faker.person.jobTitle()
 const bio = faker.lorem.lines(2)
 const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLocaleLowerCase()
@@ -11,6 +12,7 @@ const username = faker.internet.userName({ firstName: firstname, lastName: lastn
 
 <template>
     <!-- {{ fullname }} <br /> -->
+    <RouterLink :to="{name: 'CardDetails', params: {id: username}}">
     <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
         <img :src="faker.image.urlLoremFlickr({category: 'cat'})" class="rounded-t-xl"/>
         <div class="p-4">
@@ -19,4 +21,5 @@ const username = faker.internet.userName({ firstName: firstname, lastName: lastn
             <p class="mb-3 font-normal text-gray-700"> {{ bio }}</p>
         </div>
     </div>
+    </RouterLink>
 </template>
